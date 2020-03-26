86b1476b0e68b3e87d03aaf177347e6bc0de28d0
EduardoImery <ejwimery@gmail.com>
2020-03-26 15:36:13 -0600

    getting response from the server

--- router.go
+++ router.go
@@ -1,11 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 type Router struct {
-	rules := map[string]http.HandlerFunc
+	rules map[string]http.HandlerFunc
 }
 
-func NewRouter() *Router{
+func NewRouter() *Router {
 	return &Router{
-		rules : make(map[string]http.HandlerFunc),
+		rules: make(map[string]http.HandlerFunc),
 	}
+}
+
+func (r *Router) ServeHTTP(w http.ResponseWriter,
+	request *http.Request) {
+	fmt.Fprintf(w, "Hi from server")
 }
