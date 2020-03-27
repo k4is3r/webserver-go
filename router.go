5d9d1370e15c404de120d61930255685caaadc33
EduardoImery <ejwimery@gmail.com>
2020-03-26 23:42:27 -0600

    adding new func in router

--- router.go
+++ router.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
@@ -22,5 +21,12 @@ func (r *Router) FindHandler(path string) (http.HandlerFunc, bool) {
 
 func (r *Router) ServeHTTP(w http.ResponseWriter,
 	request *http.Request) {
-	fmt.Fprintf(w, "Hi from server")
+	handler, exist := r.FindHandler(request.URL.Path)
+
+	if !exist {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	handler(w, request)
 }
