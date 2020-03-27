d0b036310ab4d80a842a5eb6758e83f84c067582
EduardoImery <ejwimery@gmail.com>
2020-03-26 23:47:49 -0600

    creating a new route to map

--- handlers.go
+++ handlers.go
@@ -8,3 +8,7 @@ import (
 func HandleRoot(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi from Sever")
 }
+
+func HandleHome(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "You are in API endpoint")
+}
--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
+import "fmt"
+
 func main() {
 	server := NewServer(":3000")
+	fmt.Println("Server Running")
 	server.Handle("/", HandleRoot)
+	server.Handle("/api", HandleHome)
 	server.Listen()
 }
